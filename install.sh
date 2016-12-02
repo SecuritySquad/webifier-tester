9c494a1af7a5a8cc37797e0d338c1611cee9bd2f
samuel-p <samuel.philipp@t-online.de>
2016-12-03 00:45:00 +0100

    install file added

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+install() {
+    git clone https://github.com/SecuritySquad/$1.git
+    cd $1
+    sh install.sh
+    cd ..
+}
+
+cd ..
+
+install webifier-resolver
+
+install webifier-test-virusscan
+
+cd webifier-tester
+./gradlew :buildAll
+cd ..
+mkdir -p run
+cd run
+cp ../webifier-tester/build/libs/webifier-tester-all-*.jar .
