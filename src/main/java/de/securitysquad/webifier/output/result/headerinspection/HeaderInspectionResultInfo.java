511fb6097524138c24448840d614e6174449d4bb
dan-brown <github@djbrown.de>
2016-12-20 02:43:29 +0100

    applied new HeaderInspection output format

--- src/main/java/de/securitysquad/webifier/output/result/headerinspection/HeaderInspectionResultInfo.java
+++ src/main/java/de/securitysquad/webifier/output/result/headerinspection/HeaderInspectionResultInfo.java
@@ -3,11 +3,27 @@ package de.securitysquad.webifier.output.result.headerinspection;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import de.securitysquad.webifier.output.result.TestResultInfo;
 
-public class HeaderInspectionResultInfo {
-    @JsonProperty("ratio")
-    private int ratio;
+import java.util.List;
 
-    public int getRatio() {
-        return ratio;
+public class HeaderInspectionResultInfo {
+    @JsonProperty("meanRatio")
+    private double meanRatio;
+
+    @JsonProperty("worstRatio")
+    private double worstRatio;
+
+    @JsonProperty("browsers")
+    private List<String> browsers;
+
+    public double getMeanRatio() {
+        return meanRatio;
+    }
+
+    public double getWorstRatio() {
+        return worstRatio;
+    }
+
+    public List<String> getBrowsers() {
+        return browsers;
     }
 }
